d9e667d6f9b850ea0cf6700320a4cc66be3fa4ce
david <djunio239@gmail.com>
2023-09-20 20:39:02 -0300

    tests video close #26

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -24,7 +24,17 @@ def test_titulo_video(resp, titulo):
     assert_contains(resp, titulo)
 
 
-#def test_conteudo_video(resp):
-    #assert_contains(resp, '<iframe src="https://player.vimeo.com/video/682069825"')
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+   ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
+
+
 
 
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,16 +1,28 @@
+
 from django.shortcuts import render
+from django.urls import reverse
+
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+    def get_absolute_url(self):
+        return reverse('aperitivos:video', args=(self.slug,))
+
 
 videos = [
-      {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivacao', 'vimeo_id': 682069825},
-      {'slug': 'instalacao-windows', 'titulo': 'Instalacao Windows', 'vimeo_id': 251497668},
+      Video('motivacao', 'Video Aperitivo: Motivacao', 682069825),
+      Video('instalacao-windows', 'Instalacao Windows', 251497668),
 ]
+
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
-    return render(request, 'aperitivos/indice.html')
-
-
-videos_dct = {dct['slug']: dct for dct in videos}
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
 def video(request, slug):
