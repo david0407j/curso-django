fc56045abd5890bea92f935cff5495fe0383fe54
david <djunio239@gmail.com>
2023-10-16 22:53:37 -0300

    teste modulos close #51

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -3,7 +3,7 @@ from typing import List
 from pypro.modulos.models import Modulo
 
 
-def listar_modulos_ordenados()-> List[Modulo]:
+def listar_modulos_ordenados() -> List[Modulo]:
     """
     Lista módulos ordenados títulos
     :return:
