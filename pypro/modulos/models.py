39e367c3b8d9eddbe67310561fc4ba632a6488bc
david <djunio239@gmail.com>
2023-10-25 19:50:17 -0300

    Criar Página de Detalhamento de Aula close #54

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -25,7 +25,7 @@ class Aula(OrderedModel):
     slug = models.SlugField(unique=True)
     modulo = models.ForeignKey('Modulo', on_delete=models.PROTECT)
     order_with_respect_to = 'modulo'
-    vimeo_id = models.CharField(max_length=32)
+    vimeo_id = models.CharField(max_length=32, default='1')
 
 
 
--- pypro/settings.py
+++ pypro/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'pypro.aperitivos',
     'pypro.modulos',
     'ordered_model',
+    'django_extensions',
+
 
 
 
