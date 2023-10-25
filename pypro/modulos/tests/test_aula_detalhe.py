58fb3ff4dc7ddbb1011cad3cfdab9f91509f04e8
david <djunio239@gmail.com>
2023-10-25 20:48:00 -0300

    Criar Breadcrumb para página de Aulas close #59

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -28,5 +28,9 @@ def test_vimeo(resp, aula: Aula):
     assert_contains(resp, f'src="https://player.vimeo.com/video/{ aula.vimeo_id }"')
 
 
+def test_modulo_breadcrumb(resp, modulo):
+    assert_contains(resp, f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
+
+
 
 
